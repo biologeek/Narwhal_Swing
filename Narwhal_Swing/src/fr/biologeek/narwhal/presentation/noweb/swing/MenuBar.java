455091184ea27269882475c218b4a710d0a1cc55
xavier <xavier@xavier-CX61-2PC>
2015-04-04 01:33:14 +0200

    commit du soir... bonsoir

--- Narwhal_Swing/src/fr/biologeek/narwhal/presentation/noweb/swing/MenuBar.java
+++ Narwhal_Swing/src/fr/biologeek/narwhal/presentation/noweb/swing/MenuBar.java
@@ -1,6 +1,8 @@
 package fr.biologeek.narwhal.presentation.noweb.swing;
 
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MenuBar extends JMenuBar {
 
@@ -9,4 +11,28 @@ public class MenuBar extends JMenuBar {
 	 */
 	private static final long serialVersionUID = 7510494983268245437L;
 
+	
+	
+	JMenu menuOperations,menuParameters;
+	JMenuItem menuItemAddOperation,menuItemUpdateOperation,menuItemDeleteOperation
+	,menuItemImposition,menuItemCurrency,menuItemOptions;
+	
+	public MenuBar (){
+		this.setUpMenus();
+	}
+	
+	private void setUpMenus (){
+		
+		this.menuOperations = new JMenu("Opérations");
+		this.menuParameters = new JMenu("Paramètres");
+		this.menuItemAddOperation = new JMenuItem("Ajouter");
+		
+		this.add(menuOperations);
+		this.add(menuParameters);
+		
+		this.menuItemUpdateOperation = new JMenuItem("Modifier");
+		this.menuItemDeleteOperation = new JMenuItem("Supprimer");
+		
+		
+	}
 }
